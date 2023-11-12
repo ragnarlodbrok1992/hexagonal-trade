77c9a79ab69a4ce7562dbaa5dd37e3d8d0695e52
ragnarlodbrok1992 <maraus92@gmail.com>
2023-11-12 21:27:33 +0100

    WIP - adding shaders

--- hexagonal-trade.py
+++ hexagonal-trade.py
@@ -75,6 +75,31 @@ def main():
 
     glEnable(GL_DEPTH_TEST)
 
+     # Compile shaders and link program
+    shader = compileProgram(
+        compileShader(VERTEX_SHADER_SOURCE, GL_VERTEX_SHADER),
+        compileShader(FRAGMENT_SHADER_SOURCE, GL_FRAGMENT_SHADER)
+    )
+
+    # Create VBO and VAO
+    VBO = glGenBuffers(1)
+    glBindBuffer(GL_ARRAY_BUFFER, VBO)
+    glBufferData(GL_ARRAY_BUFFER, 4 * len(cube_vertices), (GLfloat * len(cube_vertices))(*cube_vertices), GL_STATIC_DRAW)
+
+    VAO = glGenVertexArrays(1)
+    glBindVertexArray(VAO)
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), ctypes.c_void_p(0))
+
+    # Element buffer object (EBO)
+    EBO = glGenBuffers(1)
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO)
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 4 * len(cube_indices), (GLuint * len(cube_indices))(*cube_indices), GL_STATIC_DRAW)
+
+    # Uniforms - MVP
+    model_loc = glGetUniformLocation(shader, "model")
+    view_loc = glGetUniformLocation(shader, "view")
+    projection_loc = glGetUniformLocation(shader, "projection")
+
     while not glfw.window_should_close(window):
         # Poll for and process events
         glfw.poll_events()
